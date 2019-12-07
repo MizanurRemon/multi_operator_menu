1208ad65a1a3078f1dbbf24d9fcef2013fe47e42
Remon <mr248903@gmail.com>
2019-12-07 23:23:26 +0600

    Initial Commit

---
+++ operator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  operator
+//
+//  Created by Mizanur Remon on 7/12/19.
+//  Copyright © 2019 Mizanur Remon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ operatorTests/operatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  operatorTests.swift
+//  operatorTests
+//
+//  Created by Mizanur Remon on 7/12/19.
+//  Copyright © 2019 Mizanur Remon. All rights reserved.
+//
+
+import XCTest
+@testable import operator
+
+class operatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
